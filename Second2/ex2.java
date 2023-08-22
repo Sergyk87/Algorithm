2f02b50670d7b7767829bf71c9e99547c573435c
Сергей <Sergy87@yandex.ru>
2023-08-22 23:36:09 +0300

    second2

---
+++ Second2/ex2.java
@@ -0,0 +1,73 @@
+import java.util.Date;
+import java.util.Arrays;
+
+public class ex2 {
+    public static void main(String[] args) {
+        int size = 100000;
+        int min = 0;
+        int max = 100;
+        // int[] array = ex1.getRandomArr(size, min, max);
+        // ex1.printArr(array);
+        // quickSort(array);
+        // ex1.printArr(array);
+
+        Date date = new Date();
+        int[] array = ex1.getRandomArr(size, min, max);
+        long start = new Date().getTime();
+        ex1.bubbleSort(array.clone());
+        long end = new Date().getTime();
+        System.out.println(end - start);
+        long start2 = new Date().getTime();
+        quickSort(array.clone());
+        long end2 = new Date().getTime();
+        System.out.println(end2 - start2);
+        long start3 = new Date().getTime();
+        Arrays.sort(array.clone());
+        long end3 = new Date().getTime();
+        System.out.println(end3 - start3);
+
+    }
+
+    /**
+     * @param array - исходный массив
+     * @apiNote Перегрузка метода быстрой сортировки
+     */
+    public static void quickSort(int[] array) {
+        quickSort(array, 0, array.length - 1);
+    }
+
+    /**
+     * @param array         - исходный массив
+     * @param startPosition - начало массива
+     * @param endPosition   - конец массива
+     * @apiNote Метод реализующий быструю сортировку, принимающий на вход три
+     *          аргумента
+     */
+    public static void quickSort(int[] array, int startPosition, int endPosition) {
+        int pivot = array[(startPosition + endPosition) / 2];
+        int i = startPosition, j = endPosition;
+        while (i <= j) {
+            while (array[i] < pivot) {
+                i++;
+            }
+            while (array[j] > pivot) {
+                j--;
+            }
+            if (i <= j) {
+                if (i < j) {
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+                i++;
+                j--;
+            }
+        }
+        if (i < endPosition) {
+            quickSort(array, i, endPosition);
+        }
+        if (j > startPosition) {
+            quickSort(array, startPosition, j);
+        }
+    }
+}
---
+++ Second2/ex3.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class ex3 {
+    public static void main(String[] args) {
+        int size = 100;
+        int min = 1;
+        int max = 100;
+        int[] array = ex1.getRandomArr(size, min, max);
+        Arrays.sort(array);
+        ex1.printArr(array);
+        System.out.println(search(array, 5, 0, array.length - 1));
+    }
+
+    public static int search(int[] array, int value, int min, int max) {
+        int middle;
+        if (max < min) {
+            return -1;
+        } else {
+            middle = (max - min) / 2 + min;
+        }
+        if (array[middle] < value) {
+            return search(array, value, middle + 1, max);
+        } else {
+            if (array[middle] > value) {
+                return search(array, value, min, middle - 1);
+            } else {
+                return middle;
+            }
+        }
+    }
+}
